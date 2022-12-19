c3b63e81ea41ac7b71473a2da5300eb774079916
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-19 02:25:02 +0200

    Add files via upload

---
+++ Program19.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите пятизначное число ");
+ string number = Console.ReadLine();
+ int number2 = number.Length;
+  if (number2 == 5)
+  { 
+      if(number[0] == number[4] && number[1] == number[3])
+    {
+      Console.WriteLine($"Палиндром");
+    }
+       else 
+    {
+     Console.WriteLine($"Не Палиндром"); 
+    }
+   }
+ else
+   {
+     Console.WriteLine($"НЕ пятизначное");
+        
+   }
+   {
+    
+  }
