ab6166a212aef470930b5be4d348c804de0894f1
Exstosy <47812846+Exstosy@users.noreply.github.com>
2022-12-19 02:10:56 +0200

    Add files via upload

---
+++ Program23.cs
@@ -0,0 +1,10 @@
+﻿int number = ReadInt("Ввведите N :");
+ for (int i = 1; i <= number; i++)
+  {
+    Console.WriteLine($"{i*i*i}");
+  }
+  int ReadInt(string message)
+  {
+    Console.WriteLine(message);
+     return Convert.ToInt32(Console.ReadLine());
+          }
